d9e2875ad87c01cf17447463a13fa8f7faabcb43
Jarad Solomon <jaradsolomon@gmail.com>
2022-02-19 17:02:31 -0600

    Trade create bug solved by fixing association in models

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,4 +1,8 @@
 class Artwork < ApplicationRecord
 belongs_to :user
 
+has_many :sent_offers, foreign_key: :trader_art_id, class_name: "Trade"
+has_many :received_offers, foreign_key: :vendor_art_id, class_name: "Trade"
+
+
 end
--- app/models/trade.rb
+++ app/models/trade.rb
@@ -1,8 +1,8 @@
 class Trade < ApplicationRecord
 
 belongs_to :trader, foreign_key: :trader_id, class_name: "User"
-belongs_to :trader_art, foreign_key: :trader__art_id, class_name: "Artwork"
 belongs_to :vendor, foreign_key: :vendor_id, class_name: "User"
-belongs_to :vendor_art, foreign_key: :vendor__art_id, class_name: "Artwork"
+belongs_to :trader_art, foreign_key: :trader_art_id, class_name: "Artwork"
+belongs_to :vendor_art, foreign_key: :vendor_art_id, class_name: "Artwork"
 
 end
