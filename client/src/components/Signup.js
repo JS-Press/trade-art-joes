d1bd2f38fbbb25595eeee5da145b5564b679cf5a
Jarad Solomon <jaradsolomon@gmail.com>
2022-02-22 23:01:28 -0600

    signup frontend done

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -22,33 +22,89 @@ const Signup = () => {
         <div className='signupContainer' > 
             <div className='SignUpInfo'>
                 <div className='SignUpInfoC'>
-                    <p>username</p>
-                    <input className='inputS' name='username' value={formData.username} onChange={onChangeForm} type="text"/>
-                    <br></br>
-                    <div className='SignUpInfoR'>
-                        <div className='SignUpInfoCmini' style={{marginRight:20}}>
-                            <p>first name</p>
-                            <input className='inputS' name='first_name' value={formData.username} onChange={onChangeForm} type="text"/>
+                <div className='SignUpInfoR'>
+                        <div className='SignUpInfoCmini' style={{marginRight:50}}>
+                        <p>username</p>
+                        <input className='inputS' name='username' value={formData.username} onChange={onChangeForm} type="text"/>
                         </div>
                         <div className='SignUpInfoCmini'>
-                            <p>last name</p>
-                            <input className='inputS' name='last_name' value={formData.password_digest} onChange={onChangeForm} type="text"/>
+                        <p>email</p>
+                        <input className='inputS' name='email' value={formData.email} onChange={onChangeForm} type="text"/>
                         </div>
                     </div>
                     <br></br>
-                    <p>password</p>
-                    <input className='inputS' name='password_digest' value={formData.password_digest} onChange={onChangeForm} type="password"/>
-                </div>
-                <div className='SignUpInfoC'>
-                    <p>first name</p>
-                    <input className='inputS' name='first_name' value={formData.username} onChange={onChangeForm} type="text"/>
+                    <div className='SignUpInfoR'>
+                        <div className='SignUpInfoCmini' style={{marginRight:50}}>
+                            <p>first name</p>
+                            <input className='inputS' name='first_name' value={formData.first_name} onChange={onChangeForm} type="text"/>
+                        </div>
+                        <div className='SignUpInfoCmini'>
+                            <p>last name</p>
+                            <input className='inputS' name='last_name' value={formData.last_name} onChange={onChangeForm} type="text"/>
+                        </div>
+                    </div>
                     <br></br>
-                    <p>last name</p>
-                    <input className='inputS' name='last_name' value={formData.password_digest} onChange={onChangeForm} type="text"/>
+                    <div className='SignUpInfoR'>
+                        <div className='SignUpInfoCmini' style={{marginRight:50}}>
+                        <p>password</p>
+                        <input className='inputS' name='password_digest' value={formData.password_digest} onChange={onChangeForm} type="password"/>
+                        </div>
+                        <div className='SignUpInfoCmini'>
+                        <p>confirm password</p>
+                        <input className='inputS' name='password_confirmation' value={formData.password_confirmation} onChange={onChangeForm} type="password"/>
+                        </div>
+                    </div>
+                    <br></br>
+                    <div className='SignUpInfoR'>
+                        <div className='SignUpInfoCmini' style={{marginRight:50}}>
+                        <p>profile pic link</p>
+                        <input className='inputS' name='profile_pic' value={formData.profile_pic} onChange={onChangeForm} type="text"/>
+                        </div>
+                        <div className='SignUpInfoCmini'>
+                        <p></p>
+                        <div className='inputS' style={{borderStyle:'none'}}></div>
+                        </div>
+                    </div>
+                    </div>
+                <div className='SignUpInfoC'>
+                    <p>bio</p>
+                    <input className='inputS' name='bio' value={formData.bio} onChange={onChangeForm} type="text"/>
+                    <br></br>
+                    <div className='SignUpInfoR'>
+                        <div className='SignUpInfoCmini' style={{marginRight:50}}>
+                        <p>mailing address</p>
+                        <input className='inputS' name='address' value={formData.address} onChange={onChangeForm} type="text"/>
+                        </div>
+                        <div className='SignUpInfoCmini'>
+                        <p>city</p>
+                        <input className='inputS' name='city' value={formData.city} onChange={onChangeForm} type="text"/>
+                        </div>
+                    </div>
+                    <br></br>
+                    <div className='SignUpInfoR'>
+                        <div className='SignUpInfoCmini' style={{marginRight:50}}>
+                        <p>state</p>
+                        <input className='inputS' name='state' value={formData.state} onChange={onChangeForm} type="text"/>
+                        </div>
+                        <div className='SignUpInfoCmini'>
+                        <p>zipcode</p>
+                        <input className='inputS' name='zipcode' value={formData.zipcode} onChange={onChangeForm} type="text"/>
+                        </div>
+                    </div>
+                    <br></br>
+                    <div className='SignUpInfoR' >
+                        <div className='SignUpInfoCmini'>
+                        <p></p>
+                        <div className='inputS' style={{borderStyle:'none', width: 264}}></div>
+                        </div>
+                        <div className='SignUpInfoCmini' style={{alignSelf: 'flex-end'}}>
+                        <p>art site</p>
+                        <input className='inputS' name='website' value={formData.website} onChange={onChangeForm} type="text"/>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <br></br>
-            <button className="inButton" style={{marginTop:50, position:'relative'}} >SignUp</button>
+            <button className="inButton" style={{width: 450, height: 55, marginTop:-75, position:'relative'}} >SignUp</button>
             <p className='loading' >{errors}</p>
         </div>
     );
