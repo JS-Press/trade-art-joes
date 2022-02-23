302bdf5cd7730a89b39fc02c25adaed07cadaf21
Jarad Solomon <jaradsolomon@gmail.com>
2022-02-22 21:52:19 -0600

    signup page started

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, { useState }  from "react";
+import {useNavigate} from "react-router-dom"
 
 const Signup = () => {
+
+    const navigate = useNavigate()
+    const [formData, setFormData] = useState('');
+    const [errors, setErrors] = useState('');
+
+
+  const onChangeForm = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setFormData({
+        ...formData,
+        [name]: value,
+      })
+  }
+
+
     return (
-        <div>
-            <h2>Signup!</h2>
+        <div className='signupContainer' > 
+            <div className='SignUpInfo'>
+                <div className='SignUpInfoC'>
+                    <p>username</p>
+                    <input className='inputS' name='username' value={formData.username} onChange={onChangeForm} type="text"/>
+                    <br></br>
+                    <div className='SignUpInfoR'>
+                        <div className='SignUpInfoCmini' style={{marginRight:20}}>
+                            <p>first name</p>
+                            <input className='inputS' name='first_name' value={formData.username} onChange={onChangeForm} type="text"/>
+                        </div>
+                        <div className='SignUpInfoCmini'>
+                            <p>last name</p>
+                            <input className='inputS' name='last_name' value={formData.password_digest} onChange={onChangeForm} type="text"/>
+                        </div>
+                    </div>
+                    <br></br>
+                    <p>password</p>
+                    <input className='inputS' name='password_digest' value={formData.password_digest} onChange={onChangeForm} type="password"/>
+                </div>
+                <div className='SignUpInfoC'>
+                    <p>first name</p>
+                    <input className='inputS' name='first_name' value={formData.username} onChange={onChangeForm} type="text"/>
+                    <br></br>
+                    <p>last name</p>
+                    <input className='inputS' name='last_name' value={formData.password_digest} onChange={onChangeForm} type="text"/>
+                </div>
+            </div>
+            <br></br>
+            <button className="inButton" style={{marginTop:50, position:'relative'}} >SignUp</button>
+            <p className='loading' >{errors}</p>
         </div>
     );
 }
