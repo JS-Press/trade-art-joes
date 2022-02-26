24349194740dffe42b77c54be1827ca8bb46d713
Jarad Solomon <jaradsolomon@gmail.com>
2022-02-25 21:08:23 -0600

    trade nav dropdown added

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,6 +3,8 @@ import {NavLink, Link} from "react-router-dom"
 
 function Navbar({ handleLogout, loggedIn, user }){
 
+    const [isShown, setIsShown] = useState(false);
+    console.log(isShown)
 
     return (
 <> 
@@ -12,7 +14,17 @@ function Navbar({ handleLogout, loggedIn, user }){
         <div id='nav'>
         <NavLink className='navLink' to="/about">about</NavLink>
         <NavLink className='navLink' to="/myart">my art</NavLink>
-        <NavLink className='navLink' to="/mytrades">my trades</NavLink>
+        {isShown ?
+        <div className='tradeDropdown' id='noLine' onMouseEnter={() => setIsShown(true)} onMouseLeave={() => setIsShown(false)} >
+          <NavLink className='navLink' to="/mytrades" id='noLine' >my trades</NavLink>
+          <NavLink className='tradeLink' to='/mytrades/sent' >sent</NavLink>
+          <NavLink className='tradeLink' to="/mytrades/received">received</NavLink>
+          <NavLink className='tradeLink' to="/mytrades/completed">completed</NavLink>
+        </div>
+        :<>
+        <NavLink className='navLink' to="/mytrades" onMouseEnter={() => setIsShown(true)} onMouseLeave={() => setIsShown(false)} >my trades</NavLink>
+        </>
+        }
         <Link className='navLink' id='logout' to="/" onClick={handleLogout} >logout</Link>
         <p style={{ fontSize:16, fontWeight:200, marginTop:8, marginLeft:8, borderRadius:50 }} >hi {user.first_name}!</p>
         </div>
@@ -26,7 +38,7 @@ function Navbar({ handleLogout, loggedIn, user }){
         <div id='nav'>
         <NavLink className='navLink' to="/about">about</NavLink>
         <NavLink className='inupLinks' to="/login">login</NavLink>
-        <NavLink className='inupLinks'  to="/signup">signup</NavLink>
+        <NavLink className='inupLinks' to="/signup">signup</NavLink>
         <p style={{ fontSize:18, fontWeight:200, marginTop:6, borderRadius:50, marginLeft:20 }} >Welcome!</p>
         </div>
         </>
