f79f8c02d2d2dfc63c359568aa7387556a6fb24e
Jarad Solomon <jaradsolomon@gmail.com>
2022-03-14 20:02:13 -0500

    trade sent detail finished

--- client/src/components/TradeDetailSent.js
+++ client/src/components/TradeDetailSent.js
@@ -24,21 +24,22 @@ const Tradedetailsent = () => {
         // navigate(`/users/${art.user_id}`)
     }
 
-    function handleDeleteClick(id){
+    function handleCancel(id){
         setConfirmShown(true)
         }
-
-    function handleDeleteArt(){
-        // fetch(`/artworks/${id}`, {
-        //     method: "DELETE"
-        //     }).then((r) => {
-        //     if (r.ok) {
-        //     console.log('successful delete!')
-        //     r.json().then(data => { navigate('/myArt') })
-        //     }else {
-        //     console.log('unsuccessful delete :(')
-        // }})
-    }
+        function handleDeleteOffer(){
+            fetch(`/trades/${trade.id}`, {
+                method: "DELETE"
+                }).then((r) => {
+                if (r.ok) {
+                    r.json().then(data => { 
+                        console.log('successful delete!')
+                        navigate('/tradesSent')
+                    })
+                }else {
+                console.log('unsuccessful delete :(')
+            }})
+        }
 
     return (
         <div>
@@ -56,15 +57,15 @@ const Tradedetailsent = () => {
                                 <p style={{ marginTop:12, marginLeft:5, marginRight:5, fontSize:14, fontWeight:700 ,alignSelf:'center', textAlign:'left' }}>{trade.trader_note}</p>
                             </div>
                         </div>
-                        <div style={{display:'flex', flexFlow:'column', backgroundColor:'transparent', width:'60%' }}>
+                        <div style={{display:'flex', flexFlow:'column', backgroundColor:'transparent', width:'60%', marginRight:-10 }}>
                             <img style={{ width:300, height:300}} className='reviewImgs' src={trade.vendor_art.url} alt='art image not found' onClick={()=>navigate(`/artworks/${trade.vendor_art.id}`)}/>
                             <h2 style={{fontSize:26, marginBottom:-15}}>{trade.vendor_art.title}</h2>
-                            <h4 style={{fontSize:16}}>{trade.vendor_art.tags}</h4>
+                            <h4 style={{fontSize:16 }}>{trade.vendor_art.tags}</h4>
                             <h4 style={{fontSize:16}}>{trade.vendor_art.size}"</h4>
                             <h4 style={{fontSize:16}}>{trade.vendor_art.year}</h4>
                         </div>
+                    <div className='dotted' style={{ marginTop:-9, height:470, marginBottom:-40, marginLeft:50, marginRight:-80 }}></div>
                    </div>
-                    <div className='dotted' style={{ position:'fixed', marginTop:-9, height:470, marginBottom:-40 }}></div>
                     <div style={{ display:'flex', flexFlow:'row', backgroundColor:'transparent', width:'40%' }}>
                         <div style={{display:'flex', flexFlow:'column', backgroundColor:'transparent', marginRight:50 }} >
                             <img style={{ width:300, height:300 }} className='reviewImgs' src={trade.trader_art.url} alt='art image not found' onClick={()=>navigate(`/artworks/${trade.trader_art.id}`)} />
@@ -78,17 +79,38 @@ const Tradedetailsent = () => {
                             <h3 style={{ textAlign:'left', marginBottom:-15, fontSize:26, fontWeight:800, letterSpacing:.5, backgroundColor:'transparent' }}>{trade.trader.first_name} {trade.trader.last_name}</h3>
                             {/* <h4 style={{textAlign:'left', backgroundColor:'transparent', marginLeft:0 }}>{trade.trader.street_address}</h4> */}
                             <h4 style={{textAlign:'left', backgroundColor:'transparent', marginLeft:0 }}>{trade.trader.city}, {trade.trader.state} {trade.trader.zip}</h4>
-                            <div style={{alignContent:'center', marginTop:80 }}>
-                                <p style={{marginLeft:-45, marginBottom:-10, fontWeight:300, fontSize:16 }} >offer sent to {trade.vendor.first_name} {trade.vendor.last_name}</p>
-                                <p style={{marginLeft:-45, marginBottom:-0, fontWeight:200, fontWeight:300, fontSize:16 }} >{trade.offered_date}</p>
-                                <button className='button' style={{width:220, marginTop:16}}>cancel offer</button>
-                                <p style={{marginLeft:-45, marginBottom:-0, fontWeight:500, fontSize:16 }} >waiting on a confirmation from {trade.vendor.first_name}</p>
+                            <div style={{alignContent:'flex-start', marginTop:80 }}>
+                                <p style={{marginLeft:0, marginBottom:-10, fontWeight:300, fontSize:16 }} >offer sent to {trade.vendor.first_name} {trade.vendor.last_name}</p>
+                                <p style={{marginLeft:0, marginBottom:-0, fontWeight:200, fontWeight:300, fontSize:16 }} >{trade.offered_date}</p>
+                                <button className='button' style={{ width:'100%', marginTop:16, marginLeft:'0%' }} onClick={handleCancel}>cancel offer</button>
+                                <p style={{marginLeft:0, marginBottom:-0, fontWeight:500, fontSize:16 }} >waiting on a confirmation from {trade.vendor.first_name}</p>
                             </div>
                         
                         </div>
                       </div>
                 </div>
-
+                {confirmShown? <>
+                <div className='popUp' style={{marginTop:-500, height:480, width:850}}>
+                    <button className='details' style={{marginLeft:50, marginTop:-50, fontSize:16}} onClick={()=>setConfirmShown(false)} >GO BACK</button>
+                    <br></br>
+                    <br></br>
+                    <p style={{fontSize:28}}>Cancel your offer to {trade.vendor.first_name}?</p>
+                    <p style={{fontSize:28}}>This action can't be undone :(</p>
+                    <br></br>
+                    <div style={{display:'flex', flexFlow:'row', justifyContent:'space-around', backgroundColor:'transparent' }}>
+                        <div style={{display:'flex', flexFlow:'column', backgroundColor:'transparent' }}>
+                            <button className='button' style={{width:320}} onClick={()=>setConfirmShown(false)}>no</button>
+                            <p style={{textTransform:'uppercase', fontSize:16, fontWeight:300, letterSpacing:.4}} >NEVERMIND!</p>
+                        </div>
+                        <div style={{display:'flex', flexFlow:'column', backgroundColor:'transparent' }}>
+                            <button className='button' style={{width:320}} onClick={handleDeleteOffer}>yes</button>
+                            <p style={{textTransform:'uppercase', fontSize:16, fontWeight:300, letterSpacing:.4 }} >CANCEL TRADE!</p>
+                        </div>
+                    </div>
+                </div>
+                </>:
+                <>
+                </>}
             </>:
             <>
             <p>loading...</p>
